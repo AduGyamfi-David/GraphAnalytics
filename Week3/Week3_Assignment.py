4b927ff5abf21349e73c49caafd14a59e4cb6efc
David Adu-Gyamfi <adugyamfi3141@gmail.com>
2021-06-26 21:47:05 +0100

    Final attempt to assignment 3

--- Week3/Week3_Assignment.py
+++ Week3/Week3_Assignment.py
@@ -1,29 +1,38 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 
-def bubblesort(r):
-    for i in range(0, len(r)):
-        for j in range(0, len(r) - (i + 1)):
-            if (r[j] > r[j + 1]) :
-                r[j], r[j + 1] = r[j + 1], r[j]
-    return r
+def findmax(r):
+    dict_max = ""
+    for i in range(0, len(r) - 1):
+        if (r[str(i)] > r[str(i + 1)]):
+            dict_max = (i, r[str(i)])
+    return dict_max
 
 G = nx.read_adjlist(r"C:\Users\dadug\Documents\GraphAnalytics\Week3\graph-assignment.txt")
+# G = nx.read_graphml(r"C:\Users\dadug\Documents\GraphAnalytics\Week3\assignment3_graph.graphml")
+
+weights = nx.get_edge_attributes(G, "weight")
+# print(weights)
 
 plot = nx.planar_layout(G)
-print(nx.shortest_path(G, "0", "4", method="dijkstra"))
+nx.draw_networkx(G, pos=plot)
+nx.draw_networkx_edge_labels(G, pos=plot, edge_labels=weights)
+plt.savefig(r"Week3\Graph_adjlist.png", format="PNG")
+plt.show()
+
+print(nx.dijkstra_path(G, "0", "4"))
+print(nx.dijkstra_path_length(G, "0", "4"))
 
 H = nx.breadth_first_search.bfs_tree(G, "7")
 nx.draw_networkx(H, pos=plot, arrows=True, with_labels=True)
-plt.savefig(r"Week3\BFS_Graph.png", format="PNG")
+plt.savefig(r"Week3\BFS_Graph_adjlist.png", format="PNG")
 plt.show()
 
 I = nx.depth_first_search.dfs_tree(G, "7")
 nx.draw_networkx(I, pos=plot, arrows=True, with_labels=True)
-plt.savefig(r"Week3\DFS_Graph.png", format="PNG")
+plt.savefig(r"Week3\DFS_Graph_adjlist.png", format="PNG")
 plt.show()
 
 G_pg = nx.pagerank(G)
-ranks = list(G_pg.values())
 
-print(ranks[len(ranks) - 1])
+print(findmax(G_pg))
