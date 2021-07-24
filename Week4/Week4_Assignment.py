ed5ecd624735d054aea00f4a2a3df7d5fba6ee3a
David Adu-Gyamfi <adugyamfi3141@gmail.com>
2021-07-24 21:31:07 +0100

    Solved community problem

--- Week4/Week4_Assignment.py
+++ Week4/Week4_Assignment.py
@@ -11,8 +11,10 @@ colors = [
     "lawngreen", "lightgray", "sienna", "khaki", "mistyrose"]
 
 G = nx.read_graphml(r"Week4\assignment3_graph.graphml")
+H = nx.read_graphml(r"Week4\assignment3_graph.graphml")
 
-p = nx.planar_layout(G)
+p_G = nx.planar_layout(G)
+p_H = nx.planar_layout(H)
 
 color_dict = nx.coloring.greedy_color(G, strategy="random_sequential")
 
@@ -23,6 +25,30 @@ node_colors = []
 for i in color_dict.keys():
     node_colors.append(colors[color_dict[i]])
 
-nx.draw_networkx(G, pos=p, with_labels=True, node_color=node_colors)
+print(node_colors)
+
+nx.draw_networkx(G, pos=p_G, with_labels=True, node_color=node_colors)
 plt.savefig(r"Week4\colored_graph.png", format="PNG")
+plt.show()
+
+coms = girvan_newman(H)
+node_coms = []
+
+# print(coms)
+# print(type(coms))
+
+for community in next(coms):
+    # print(community)
+    node_coms.append(community)
+
+coms_node_colors = []
+for nc in range(len(node_coms)):
+    col = random.randint(0, len(colors) - 1)
+    print(col)
+    for node in H:
+        if node in node_coms[nc]:
+            coms_node_colors.append(colors[col])
+
+nx.draw_networkx(H, pos=p_H, node_color=coms_node_colors)
+plt.savefig(r"Week4\community_graph.png", format="PNG")
 plt.show()
